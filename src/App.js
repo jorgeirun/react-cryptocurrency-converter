ce7e9025ff5c12551706d707610550ae907fee8f
Jorge <jorgemiv2@gmail.com>
2024-07-12 16:11:07 -0400

    Data Fetch and UI update
    Updated page title in index.html
    Added url fetch and update dropdown with response
    Error handling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import DataFetcher from './DataFetcher';
 import './App.css';
 
 function App() {
+
+  const [symbols, setSymbols] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const handleDataFetched = (fetchedData) => {
+    if (fetchedData.error) {
+      setError(fetchedData.error);
+    } else {
+      const symbolsList = fetchedData.data.map(coin => ({
+        symbol: coin.symbol,
+        price_usd: coin.metrics.market_data.price_usd,
+      }));
+      setSymbols(symbolsList);
+    }
+    setLoading(false);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DataFetcher onDataFetched={handleDataFetched} />
+        {loading && <p>Loading..</p>}
+        {error && <p>Error: {error}</p>}
+        <form>
+          {!loading && !error && (
+            <div>
+              <h1>Crypto Converter</h1>
+              <h5>Select a Coin</h5>
+              <select id="symbolSelect">
+                <option>--</option>
+                {symbols.map((coin, index) => (
+                  <option key={index} value={coin.price_usd}>
+                    {coin.symbol}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
+          <br />
+          <input type="text" name="crypto_value" placeholder="1.234" value="1" />
+          <br />
+          <input type="text" name="usd_value" placeholder="1,234.56" />
+        </form>
       </header>
     </div>
   );
---
+++ src/DataFetcher.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+
+function DataFetcher({ onDataFetched }) {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("https://data.messari.io/api/v1/assets?fields=symbol,metrics/market_data/price_usd");
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                onDataFetched(data);
+            } catch (error) {
+                onDataFetched({ error: error.message });
+            }
+        };
+        fetchData();
+    });
+
+    return null;
+}
+
+export default DataFetcher;
